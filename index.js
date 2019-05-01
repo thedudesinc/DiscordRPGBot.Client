a3e2317f4a5882820e03dfeb606d91ddea02eafd
Nate Pruessner <npruessner@protonmail.com>
2019-05-01 14:44:57 -0500

    Moved foleders to the root

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const cooldowns = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
